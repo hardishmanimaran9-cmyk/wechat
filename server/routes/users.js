04187b09814f5ab8dc9d8ef607a14b81f05ac572
hardish <hardishmanimaran9@gmail.com>
2026-04-11 15:07:23 +0530

    feat: Add profile picture support to backend

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,7 +5,7 @@
 
 const express = require("express");
 const router = express.Router();
-const { searchUsers, getFriends, getMe } = require("../controllers/userController");
+const { searchUsers, getFriends, getMe, updateProfilePicture } = require("../controllers/userController");
 const authMiddleware = require("../middleware/auth");
 
 // All routes below require authentication
@@ -17,5 +17,8 @@ router.get("/friends", authMiddleware, getFriends);
 
 // GET /api/users/me
 router.get("/me", authMiddleware, getMe);
+
+// PUT /api/users/profile-picture
+router.put("/profile-picture", authMiddleware, updateProfilePicture);
 
 module.exports = router;
